c2f013b7c067cd1fe0d8e9b4598a56b33b49e07c
Akshay Upadhyay <akshayu383@gmail.com>
2020-07-19 09:31:38 +0530

    Update 3.0

--- app/src/main/java/com/akshaytech/tictactoe/StartActivity.java
+++ app/src/main/java/com/akshaytech/tictactoe/StartActivity.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class StartActivity extends AppCompatActivity {
     EditText editText1,editText2;
     String firstName,secondName;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        button = findViewById(R.id.btnSubmit);
         editText1 = findViewById(R.id.firstName);
         editText2 = findViewById(R.id.secondName);
 
@@ -21,6 +26,8 @@ public class StartActivity extends AppCompatActivity {
     public void Submit(View view) {
         firstName = editText1.getText().toString();
         secondName = editText2.getText().toString();
+        Animation animation = AnimationUtils.loadAnimation(StartActivity.this,R.anim.bounce);
+        button.startAnimation(animation);
         Intent intent = new Intent(StartActivity.this,MainActivity.class);
         intent.putExtra("first",firstName);
         intent.putExtra("second",secondName);
